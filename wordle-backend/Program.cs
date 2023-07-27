c5960928cf1c6bfc6f8989b9c287c24a8f08ecd9
Utsav <63189087+utsavj@users.noreply.github.com>
2023-07-27 23:30:45 +0530

    add asp .net connected to mariaDB

---
+++ wordle-backend/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContextPool<MariaDbContext>(options => options
+    .UseMySql(
+        builder.Configuration.GetConnectionString("MariaDbConnectionString"),
+        new MySqlServerVersion(new Version(10, 5, 4))
+    )
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
